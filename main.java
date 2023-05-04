abee6d3330d8a13cb063710ca9ed134d2eba6022
Gustavo Beche Lopes <gustavo.beche@gmail.com>
2023-05-04 21:22:02 +0000

    add base

--- main.java
+++ main.java
@@ -1,13 +1,30 @@
 class Main {
     public static void main(String[] args) {
-        
+        System.out.println(search("asdahdfjhsjdfhuisatusvf3fdgfgdfgxzcvgzdfkszfgsf3", "f3fdgfgdfg"));
     }
 
-    public String createString(int n) {
+    public static String createString(int n) {
         return "";
     }
 
-    public int search(String str, String search) {
+    public static int search(String str, String search) {
+        int iteracoes = 0;
+        int lenSearch = search.length();
+        
+        if (lenSearch > str.length()) {
+            System.out.println("iteracoes: "+ iteracoes);
+            return -1;
+        }
+
+        for (int x = 0; x <= str.length() - lenSearch; x++) {
+            if (search.equals(str.substring(x, x+lenSearch))) {
+                System.out.println("iteracoes: "+ iteracoes);
+                return x;
+            }
+            iteracoes++;
+        }
+        
+        System.out.println("iteracoes: "+ iteracoes);
         return -1;
     }
 }
