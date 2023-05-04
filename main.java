53d3dda97aa63dbfa1ca1e4e5cff6f0e48dd4344
Gustavo Beche Lopes <gustavo.beche@gmail.com>
2023-05-04 21:05:18 +0000

    Fist commit feliz

---
+++ main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public String createString(int n) {
+        return "";
+    }
+
+    public int search(String str, String search) {
+        return -1;
+    }
+}
